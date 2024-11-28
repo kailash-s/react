b54964f6b06b227fdf8e0a17f2e944f512f49005
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2024-11-28 06:42:52 +0000

    intro to jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,30 @@
+// NOTES
+ // Code below can be used with interpolation
+ const name = 'Harry';
+ const x = 10;
+ const y = 20;
+ const names = ['Pi', 'Bi', 'Fi', 'Di'];
+ const loggedIn = false;
+
+// The below code goes in the return
+<>
+  <div className="text-5xl">App</div>
+  {/* Inline styling can be used as well like below */}
+  <p style={{ color: 'red' }}>Hello { name }</p>
+  <p>
+    The sum of {x} and {y} is {x+y}
+  </p>
+  <ul>
+    { names.map((name, index) => (
+      <li key={index}>{ name }</li>
+    )) }
+  </ul>
+  {loggedIn ? <h1>Hello member</h1> : <h1>Hello guest</h1>}
+</>
+
+
 function App() {
-  return(
-    <div className="text-5xl">App</div>
-  );
+ 
 }
 
 export default App
